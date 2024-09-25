4a42a7fa054100612547a06523cfb571a9d87d63
sakshammaggu <sakshammaggu80@gmail.com>
2024-09-25 13:40:54 +0530

    creating server and routes to function handler

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func formHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+
+}
 
 func main() {
 	fmt.Println("Simple go web server")
+
+	fileServer:=http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/hello", helloHandler)
+
+	fmt.Println("Server starting at server 8080")
+
+	err:=http.ListenAndServe(":8080", nil)
+	if (err!=nil) {
+		log.Fatal(err)
+	}
 }
